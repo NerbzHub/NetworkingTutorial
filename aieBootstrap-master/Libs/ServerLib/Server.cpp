454cb7c6278cda94c6d70da7362a0481599437c4
DESKTOP-NKLI64R\N <nathan.nette@gmail.com>
2018-05-08 20:19:05 +1000

    FINISHED COMMENTING

--- aieBootstrap-master/Libs/ServerLib/Server.cpp
+++ aieBootstrap-master/Libs/ServerLib/Server.cpp
@@ -79,15 +79,21 @@ void Server::handleNetworkMessages(RakNet::RakPeerInterface * pPeerInterface)
 */
 void Server::sendNewClientID(RakNet::RakPeerInterface * pPeerInterface, RakNet::SystemAddress & address)
 {
-	
+	// Create a new instance of a BitStream.
 	RakNet::BitStream bs;
+
+	// Write a message to identify what the packet is.
 	bs.Write((RakNet::MessageID)ID_SERVER_SET_CLIENT_ID);
+
+	// Write the next client Id number.
 	bs.Write(nextClientID);
+
+	// Add 1 to the client ID for the next client.
 	nextClientID++;
 
+	// Send the packet to the server.
 	pPeerInterface->Send(&bs, HIGH_PRIORITY, RELIABLE_ORDERED,
 		0, address, false);
-
 }
 
 /**
@@ -97,29 +103,40 @@ void Server::sendNewClientID(RakNet::RakPeerInterface * pPeerInterface, RakNet::
 */
 std::string Server::getIPAddress()
 {
+	// A new string to store the IPv4 of the network this pc is on.
 	std::string line;
-	std::ifstream IPFile;
-	int offset;
-	const char* search0 = "IPv4 Address. . . . . . . . . . . :";      // search pattern
 
+	// This is for the ipconfig text to be saved to.
+	std::ifstream IPFile;
+
+	// An offset for how many lines to go down in the txt file.
+	int offset;
+
+	// This is used to search for this series of characters because the IPv4 is after it.
+	const char* search0 = "IPv4 Address. . . . . . . . . . . :";
+
+	// Inputs ipconfig into console to get the network data of the PC. Then writes it to ip.txt.
 	system("ipconfig > ip.txt");
 
+	// Open the file just created.
 	IPFile.open("ip.txt");
+
+	/* Check if the file is open or not. If it is, search through the text file to find
+		search0 to then extract the IPv4 address from the text file.*/
 	if (IPFile.is_open())
 	{
 		while (!IPFile.eof())
 		{
 			getline(IPFile, line);
 			if ((offset = line.find(search0, 0)) != std::string::npos)
-			{
-				//   IPv4 Address. . . . . . . . . . . : 1
-				//1234567890123456789012345678901234567890     
+			{    
 				line.erase(0, 39);
 				std::cout << line;
 			}
 		}
 	}
 
+	// Return the IPv4 address of the network, extracted from the .txt file.
 	return line;
 }
 
@@ -132,12 +149,20 @@ void Server::sendClientPing(RakNet::RakPeerInterface * pPeerInterface)
 {
 	while (true)
 	{
+		// Create a new BitStream instance.
 		RakNet::BitStream bs;
+
+		// Write a message to identify what's inside this packet.
 		bs.Write((RakNet::MessageID)ID_SERVER_TEXT_MESSAGE);
+
+		// Console output Ping!.
 		bs.Write("Ping!");
 
+		// Send the packet across the network. 
 		pPeerInterface->Send(&bs, HIGH_PRIORITY, RELIABLE_ORDERED, 0,
 			RakNet::UNASSIGNED_SYSTEM_ADDRESS, true);
+
+		// Sleep just this thread for a second so that it only pings every second.
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 }
