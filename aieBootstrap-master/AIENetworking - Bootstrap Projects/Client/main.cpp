84e0dfb788c773785b9a0bcf70375877e72fad75
NerbzHub <nathan.nette@gmail.com>
2018-05-08 13:35:10 +1000

    Finished main.cpp of client comments.

--- aieBootstrap-master/AIENetworking - Bootstrap Projects/Client/main.cpp
+++ aieBootstrap-master/AIENetworking - Bootstrap Projects/Client/main.cpp
@@ -1,13 +1,27 @@
+/**
+	main.cpp
+	Purpose: Calling the application on run, setting the window size
+			 and deleting the app afterwards.
+
+	@author Nathan Nette
+*/
 #include "Client.h"
 
-//extern "C"
-//{
-	int main() {
+/**
+	Main calls the Client application and once the application is closed,
+	it deletes the application to deallcate the memory.
+*/
+int main()
+{
+	// Create new Client instance;
+	auto app = new Client();
 
-		auto app = new Client();
-		app->run("AIE", 1280, 720, false);
-		delete app;
+	// Run the Client instance at 1280 by 720.
+	app->run("AIE", 1280, 720, false);
 
-		return 0;
-	}
-//}
+	// Delete the application to deallocate the memory.
+	delete app;
+
+	// Int functions need to return a value.
+	return 0;
+}
